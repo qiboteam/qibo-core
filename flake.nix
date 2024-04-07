be50b8ba69ea3586a85c9ade08a2373c2cd79aea
Alessandro Candido <candido.ale@gmail.com>
2024-04-07 09:58:01 +0200

    feat: Scaffold python extension

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
 
           modules = [
             {
-              packages = with pkgs; [pre-commit poethepoet stdenv.cc.cc.lib];
+              packages = with pkgs; [pre-commit maturin stdenv.cc.cc.lib];
 
               languages.python = {
                 enable = true;
