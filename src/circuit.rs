e41625f2d386db31b9fc3371679d4c142fd30d95
Alessandro Candido <candido.ale@gmail.com>
2024-04-08 18:11:17 +0200

    feat: Start designing circuits

---
+++ src/circuit.rs
@@ -0,0 +1,29 @@
+use crate::gate::Gate;
+
+/// A discrete gate-based representation of a quantum computation.
+///
+/// The circuit is represented as an unstrtuctured set of gates, with their connectivity separately
+/// recorded by an adjacency list.
+/// Moreover, a circuit is not a random graph, but rather a set of wires, with possible links
+/// across them. This is represented by recording the circuit ends, where it is possible to append
+/// further gates, including measurements. They identify the quantum elements (local subsystem)
+/// where the gates are acting on.
+#[derive(Debug)]
+pub struct Circuit {
+    /// Set of gates
+    gates: Vec<Gate>,
+    /// Gates connectivity
+    edges: Vec<(usize, usize)>,
+    /// Current final gates of each wire
+    ends: Vec<Option<usize>>,
+}
+
+impl Circuit {
+    pub fn new(elements: i32) -> Self {
+        Circuit {
+            gates: vec![],
+            edges: vec![],
+            ends: vec![None; qubits],
+        }
+    }
+}
