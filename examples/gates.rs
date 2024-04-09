d02cebf2e4e628d02cac4b618966ae71b04c0dae
Alessandro Candido <candido.ale@gmail.com>
2024-04-09 17:42:50 +0200

    test: Update examples

--- examples/gates.rs
+++ examples/gates.rs
@@ -1,7 +1,11 @@
 use qibo_core::prelude::*;
 
 fn main() {
-    let gates: Vec<Gate> = vec![(X {}).into(), (RX { theta: 1.5 }).into(), (H {}).into()];
+    let gates: Vec<Gate> = vec![
+        Gate::One(One::X),
+        Gate::One(One::RX(1.5)),
+        Gate::One(One::H),
+    ];
 
     println!("{:#?}", gates);
 }
