7c471a1bef2dde181dbaa7f8adb419276a7a67db
Alessandro Candido <candido.ale@gmail.com>
2024-04-09 13:15:26 +0200

    feat: Sketch a minimal draw function, to help debugging circuits

--- examples/circuit.rs
+++ examples/circuit.rs
@@ -1,7 +1,10 @@
 use qibo_core::prelude::*;
 
 fn main() {
-    let circuit = Circuit::new(5);
+    let mut circuit = Circuit::new(5);
+    circuit.add((X {}).into(), 2);
+    circuit.add((Y {}).into(), 2);
+    circuit.add((Y {}).into(), 4);
 
-    println!("{:#?}", circuit);
+    println!("{}", circuit);
 }
