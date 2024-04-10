92d13edec02a66b6266491e10269b8b68e4de2cf
Alessandro Candido <candido.ale@gmail.com>
2024-04-10 16:55:38 +0200

    feat: Upgrade example to 2-elements gates
    By now failing, but at runtime, so the interface is already fine

--- examples/circuit.rs
+++ examples/circuit.rs
@@ -2,10 +2,11 @@ use qibo_core::prelude::*;
 
 fn main() {
     let mut circuit = Circuit::new(5);
-    circuit.add(X.into(), 2);
-    circuit.add(Y.into(), 2);
-    circuit.add(H.into(), 2);
-    circuit.add(Y.into(), 4);
+    circuit.add(X.into(), vec![2]);
+    circuit.add(Y.into(), vec![2]);
+    circuit.add(H.into(), vec![2]);
+    circuit.add(CNOT.into(), vec![2, 1]);
+    circuit.add(Y.into(), vec![4]);
 
     println!("{}", circuit);
 }
