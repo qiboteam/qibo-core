adba8ef53c1006437d89b69b6ba46933f52c1211
Alessandro Candido <candido.ale@gmail.com>
2024-04-14 18:10:57 +0200

    feat: Change Python's gate representation

--- qibo-core-py/src/gate.rs
+++ qibo-core-py/src/gate.rs
@@ -1,33 +1,38 @@
 use pyo3::prelude::*;
-use qibo_core::prelude;
+use qibo_core::prelude::{self, More, One, Two};
 
 #[pymodule]
 pub mod gate {
+
     use super::*;
-
-    #[pyfunction]
-    fn X() -> Gate {
-        Gate((prelude::X {}).into())
-    }
-
-    #[pyfunction]
-    fn RX(angle: f64) -> Gate {
-        Gate((prelude::RX(angle)).into())
-    }
-
-    #[pyfunction]
-    fn CNOT() -> Gate {
-        Gate((prelude::CNOT {}).into())
-    }
 
     #[pyclass]
     #[derive(Clone)]
-    pub struct Gate(pub(crate) prelude::Gate);
+    pub enum Gate {
+        X {},
+        Y {},
+        RX { angle: f64 },
+        CNOT {},
+        TOFFOLI {},
+    }
+
+    impl Gate {
+        pub(crate) fn gate(&self) -> prelude::Gate {
+            match &self {
+                &Self::X {} => One::X.into(),
+                &Self::Y {} => One::Y.into(),
+                &Self::RX { angle } => One::RX(*angle).into(),
+                &Self::CNOT {} => Two::CNOT.into(),
+                &Self::TOFFOLI {} => More::TOFFOLI.into(),
+            }
+        }
+    }
 
     #[pymethods]
     impl Gate {
+        #[getter]
         fn elements(&self) -> usize {
-            self.0.elements()
+            self.gate().elements()
         }
     }
 }
