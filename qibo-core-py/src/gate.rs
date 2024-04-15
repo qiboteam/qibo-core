771cc850b1c06bb644ec31be622cd24d25e4a1ba
Alessandro Candido <candido.ale@gmail.com>
2024-04-15 11:04:44 +0200

    feat: Bootstrap qibo replacement, supporting QFT model

--- qibo-core-py/src/gate.rs
+++ qibo-core-py/src/gate.rs
@@ -9,20 +9,28 @@ pub mod gate {
     #[pyclass]
     #[derive(Clone)]
     pub enum Gate {
+        H {},
         X {},
         Y {},
+        Z {},
         RX { angle: f64 },
         CNOT {},
+        CU1 { angle: f64 },
+        SWAP {},
         TOFFOLI {},
     }
 
     impl Gate {
         pub(crate) fn gate(&self) -> prelude::Gate {
             match &self {
+                &Self::H {} => One::H.into(),
                 &Self::X {} => One::X.into(),
                 &Self::Y {} => One::Y.into(),
+                &Self::Z {} => One::Z.into(),
                 &Self::RX { angle } => One::RX(*angle).into(),
                 &Self::CNOT {} => Two::CNOT.into(),
+                &Self::CU1 { angle } => Two::CU1(*angle).into(),
+                &Self::SWAP {} => Two::SWAP.into(),
                 &Self::TOFFOLI {} => More::TOFFOLI.into(),
             }
         }
--- src/gate/two.rs
+++ src/gate/two.rs
@@ -3,12 +3,16 @@ use std::fmt::{self, Display};
 #[derive(Debug, Clone, Copy)]
 pub enum Two {
     CNOT,
+    CU1(f64),
+    SWAP,
 }
 
 impl Display for Two {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(match self {
             Self::CNOT => "X",
+            Self::CU1(_) => "U1",
+            Self::SWAP => "x",
         })
     }
 }
