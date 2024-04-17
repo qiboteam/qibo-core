07af61ca1ddf7cfefa964b6b97c2dbe2ae6bfbbe
Alessandro Candido <candido.ale@gmail.com>
2024-04-17 10:23:19 +0200

    feat: Add backend execution placeholder

--- src/backend.rs
+++ src/backend.rs
@@ -1,15 +1,16 @@
-use std::io;
-use std::net::TcpListener;
+use std::io::{self, Read, Write};
+use std::net::{TcpListener, TcpStream};
 use std::process::Command;
 
 const PREFIX: &str = "qibo-backend";
+const LOCAL: &str = "127.0.0.1";
 
 #[derive(Debug)]
 struct Port(u16);
 
 impl Port {
     fn available(&self) -> bool {
-        TcpListener::bind(("127.0.0.1", self.0)).is_ok()
+        TcpListener::bind((LOCAL, self.0)).is_ok()
     }
 
     fn new() -> Result<Self, ()> {
@@ -44,5 +45,20 @@ impl Backend {
 
     pub fn connect(port: u16) -> Self {
         Self { port: port.into() }
+    }
+
+    fn address(&self) -> String {
+        let port = self.port.0;
+        format!("{LOCAL}:{port}")
+    }
+
+    pub fn execute(&self, a: u8) -> io::Result<String> {
+        let mut stream = TcpStream::connect(self.address())?;
+        let mut buffer = [0; 10];
+
+        stream.write(&[a])?;
+        stream.read(&mut buffer[..])?;
+
+        Ok(std::str::from_utf8(&buffer).unwrap().to_owned())
     }
 }
