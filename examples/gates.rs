a3800745feea9251848e714948444055eb0364e5
Alessandro Candido <candido.ale@gmail.com>
2024-04-22 12:56:31 +0200

    feat: Start (de)serializing gates

--- examples/gates.rs
+++ examples/gates.rs
@@ -1,7 +1,10 @@
 use qibo_core::prelude::*;
 
+use serde::Serialize;
+
 fn main() {
     let gates: Vec<Gate> = vec![X.into(), RX(1.5).into(), H.into()];
 
+    let serialized = serde_json::to_string(&gates).unwrap();
     println!("{:#?}", gates);
 }
