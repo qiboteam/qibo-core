b68dcac0ac72470426c26f9162ba35e3f9813117
Alessandro Candido <candido.ale@gmail.com>
2024-04-25 15:35:57 +0200

    fix: Fix outdated example, amend wrong project url

--- qibo-core-py/examples/gate.py
+++ qibo-core-py/examples/gate.py
@@ -3,7 +3,7 @@ from qibo_core import gate
 # waiting for PyO3 v0.22
 # https://github.com/PyO3/pyo3/issues/3900
 # https://github.com/PyO3/pyo3/issues/759
-X = gate.X
-RX = gate.RX
+X = gate.Gate.X
+RX = gate.Gate.RX
 
 print([X() if i % 2 == 0 else RX(i) for i in range(7)])
