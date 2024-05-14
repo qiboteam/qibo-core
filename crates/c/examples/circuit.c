41e6b5cfb0c470d8ef174f3eda2ffe963e39e4cc
Stefano Carrazza <stefano.carrazza@cern.ch>
2024-05-14 10:55:39 +0200

    adding deps

--- crates/c/examples/circuit.c
+++ crates/c/examples/circuit.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-// #include "qibo_core_c.h"
+#include "qibo_core_c.h"
 
 int main(int argc, char *argv[])
 {
+	qibo_core_circuit *c = qibo_core_circuit_new(5);
+
 	printf("ciao\n");
 	return 0;
 }
