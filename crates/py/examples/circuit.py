f3de01a49ce1a8b06e00dea15773ed6ab2374210
Alessandro Candido <candido.ale@gmail.com>
2024-06-20 18:44:39 +0200

    fix: Restore Python circuit example

---
+++ crates/py/examples/circuit.py
@@ -0,0 +1,21 @@
+from qibo_core import Circuit, gates, NumpyBackend
+
+
+c = Circuit(5)
+c.add(gates.X(), [2])
+c.add(gates.RX(3.0), [2])
+c.add(gates.CNOT(), [0, 2])
+c.add(gates.X(), [1])
+c.add(gates.X(), [3])
+c.add(gates.Y(), [3])
+c.add(gates.X(), [2])
+c.add(gates.Y(), [4])
+c.add(gates.CNOT(), [4, 1])
+c.add(gates.X(), [0])
+
+print(c)
+
+backend = NumpyBackend()
+result = backend.execute_circuit(c)
+print()
+print(result)
