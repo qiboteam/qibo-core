79d95868fadf724ccd0d0f3274d819a27d1d0a7c
Alessandro Candido <candido.ale@gmail.com>
2024-06-24 13:46:40 +0200

    feat: Split draw and execution, add infrastructure to compile everything
    Install microsimulator as a library in temporary prefix, everything manually... unfortunately 😓

--- crates/c/examples/draw.c
+++ crates/c/examples/draw.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <complex.h>
 #include "qibo_core_c.h"
-#include "microsim.h"
+// #include "microsim.h"
 
 
 void print_state(complex double *state, const size_t size) {
-    //const size_t size = sizeof(state) / sizeof(state[0]);
-    for (size_t i=0; i < size; i++) {
-        printf("%ld: %.4f, %.4f\n", i, creal(state[i]), cimag(state[i]));
-    }
+	//const size_t size = sizeof(state) / sizeof(state[0]);
+	for (size_t i=0; i < size; i++) {
+		printf("%ld: %.4f, %.4f\n", i, creal(state[i]), cimag(state[i]));
+	}
 }
 
 
@@ -22,16 +22,16 @@ int main(int argc, char *argv[])
 	qibo_core_circuit_add(c, "CNOT", (size_t[]) {0, 3}, 2);
 
 	printf("%s\n", qibo_core_circuit_draw(c));
-	
+
 	// Initialize 5-qubit state vector
-    complex double state[32] = { 0 };
+	complex double state[32] = { 0 };
 	state[0] = 1;
-	
+
 	// Execute circuit
-	execute_circuit(c, state);
+	// execute_circuit(c, state);
 
 	// Print state vector
-	print_state(state, 32);
-	
+	// print_state(state, 32);
+
 	return 0;
 }
